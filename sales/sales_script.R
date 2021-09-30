cf7f16e608c7899b223acf157a25e98a0637b953
NataliePegg <nataliepegg117@gmail.com>
2021-09-30 12:26:19 -0400

    Upload the updated R script

--- sales/sales_script.R
+++ sales/sales_script.R
@@ -2,3 +2,7 @@
 
 # use rproject to create hard link to the sales folder 
 sales_data <- read.csv("sales/202003_sales.csv")
+
+# notice that there is a repository so new git folder
+# question marks symbolized un-tracked changes
+# click on path and click "commit" button, then add comment
